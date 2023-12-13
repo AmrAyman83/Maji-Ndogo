e1714f3cffdc331c1aee0bcc2426c36016e6780e
Amr_Ayman <124441372+AmrAyman83@users.noreply.github.com>
2023-12-13 22:54:28 +0200

    Add files via upload

---
+++ file 6.sql
@@ -0,0 +1,15 @@
+SELECT
+location.address,
+location.town_name,
+location.province_name,
+water_source.source_id,
+water_source.type_of_water_source,
+well_pollution.results
+FROM
+water_source
+LEFT JOIN
+well_pollution ON water_source.source_id = well_pollution.source_id
+INNER JOIN
+visits ON water_source.source_id = visits.source_id
+INNER JOIN
+location ON location.location_id = visits.location_id
