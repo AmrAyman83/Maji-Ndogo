2142fbf84d6c69d5b08c89c5132a5ab68b1140b6
Amr_Ayman <124441372+AmrAyman83@users.noreply.github.com>
2023-12-13 22:52:01 +0200

    Add files via upload

---
+++ file 3.sql
@@ -0,0 +1,23 @@
+CREATE VIEW combined_analysis_table AS
+/* This view assembles data from different tables into one to simplify analysis */
+SELECT
+water_source.type_of_water_source AS source_type,
+location.town_name,
+location.province_name,
+location.location_type,
+water_source.number_of_people_served AS people_served,
+visits.time_in_queue,
+well_pollution.results
+FROM
+visits
+LEFT JOIN
+well_pollution
+ON well_pollution.source_id = visits.source_id
+INNER JOIN
+location
+ON location.location_id = visits.location_id
+INNER JOIN
+water_source
+ON water_source.source_id = visits.source_id
+WHERE
+visits.visit_count = 1;
